6addc73f790462d325577d41e954e13bcea1b41f
Chanduchan96 <116905272+Chanduchan96@users.noreply.github.com>
2023-02-14 07:09:33 +0530

    02-14-2023

--- script.js
+++ script.js
@@ -4,29 +4,62 @@ let myLeads=[];
 const inputButton=document.getElementById('input-btn');
 const inputEl=document.getElementById('input-el');
 const ulEl=document.getElementById('ul-el');
+const deleteButton=document.getElementById('delete-btn');
+const tabButton=document.getElementById('tab-btn');
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem('myLeads'));
 
-let leadsFromLocalStorage=JSON.parse(localstorage.getItem('myLeads'));
+//checking for localstorage to check for truthy or falsy
+if(leadsFromLocalStorage)
+{
+    myLeads=leadsFromLocalStorage;
+    render(myLeads);
+}
 
-inputButton.addEventListener("click",function(){
-    console.log('in Event Listener');
-     myLeads.push(inputEl.value);
-     inputEl.value="";
-
-     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-     renderLeads();
-
-});
-function renderLeads(){
+//used to put values in the DOM
+function render(leads){
     let listItems="";
-for(let leads of myLeads){
+for(let lead of leads){
     
     listItems+=`
     <li> 
-    <a target='_blank' href='"${leads}"'>"${leads}"
+    <a target='_blank' href='"${lead}"'>"${lead}"
     </a> 
     </li>`;
     
 }
-ulEl.innerHTML=listItems;
+ulEl.innerHTML=listItems;//as best practice DOM items should be outside the loop
 
 }
+
+
+//listn clicks in tab button
+tabButton.addEventListener('click',function(){
+    //to talk to chrome API use the following
+    //chrome.tab is an chrome API used to talk to existing tab data
+    chrome.tabs.query({active:true, currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads',JSON.stringify(myLeads));
+        render(myLeads);
+    });
+  
+
+
+});
+//function save button clivk
+inputButton.addEventListener("click",function(){
+   
+     myLeads.push(inputEl.value);
+     inputEl.value="";
+
+     localStorage.setItem("myLeads",JSON.stringify(myLeads));
+     render(myLeads);
+
+});
+
+//function on delte button click
+deleteButton.addEventListener("dblclick",function(){
+        localStorage.clear();
+        myLeads=[];
+        render(myLeads);//to clear the DOM since myLEads array is set to null
+
+});
